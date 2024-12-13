1c428d15d0bd4b7846c317f361a146a69ab5b4e6
Igwe Ugochukwu Michael Sylvester <elinteerie@gmail.com>
2024-12-14 00:10:47 +0100

    assed

--- routers/airdrop.py
+++ routers/airdrop.py
@@ -7,15 +7,21 @@ from fastapi.templating import Jinja2Templates
 
 
 
-router = APIRouter(prefix='/home',tags=['Home'])
+router = APIRouter(prefix='',tags=['Home'])
 
 templates = Jinja2Templates(directory="templates")
 
 
-@router.get("", response_class=HTMLResponse)
+@router.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse(
         request=request, name="index.html")
+
+
+@router.get("/airdrop/login", response_class=HTMLResponse)
+async def airdrop_login(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="login.html")
 
 
 @router.get("/h", status_code=status.HTTP_200_OK)
