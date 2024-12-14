1ddea4a3c8ba1d52dc20f348cbcf11fa1111c144
Igwe Ugochukwu Michael Sylvester <elinteerie@gmail.com>
2024-12-14 02:33:15 +0100

    modified schema

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ from enum import Enum
 class User(SQLModel, table=True):
     id: int = Field(index=True, primary_key=True)
     wallet: str = Field(unique=True)
-    points: float = Field(default=0.0)
+    points: float = Field(default=1000.0)
     referral_id: str = Field(nullable=True)
     referral_count: int =Field(default=0)
     tasks: int = Field(default=0)
